301918cab07929288fc1f25bd39e792d09bb25f7
sskaler <kaler_simr06@hotmail.com>
2014-03-27 23:54:20 -0600

    Added 6 tables

--- ExcelAddIn3/ExcelAddIn3/UserControl1.cs
+++ ExcelAddIn3/ExcelAddIn3/UserControl1.cs
@@ -25,10 +25,256 @@ namespace ExcelAddIn3
             splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             
         }
+
+        private void Get_PlantOperatingHoursData()
+        {
+            int row = 1;
+            int col = 1;
+
+            int col1;
+            int col2;
+            float col3;
+            string col4;
+            string col5;
+            string col6;
+            string col7;
+            string col8;
+
+            var sheet1 = Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets["Sheet1"];
+            col = 44;
+            row = 3;
+            string value = Convert.ToString(sheet1.cells[row, col].value2);
+            connection();
+            connection_open();
+            while (value != null)
+            {
+                //Get values from excel cells and convert them to appropriate type (should match with column type in database)
+                col1 = Convert.ToInt32(value);
+                col2 = Convert.ToInt32(sheet1.cells[row, col + 1].value2);
+                col3 = Convert.ToSingle(sheet1.cells[row, col + 2].value2);
+                col4 = Convert.ToString(sheet1.cells[row, col + 3].value2);
+                col5 = Convert.ToString(sheet1.cells[row, col + 4].value2);
+                col6 = Convert.ToString(sheet1.cells[row, col + 5].value2);
+                col7 = Convert.ToString(sheet1.cells[row, col + 6].value2);
+                col8 = Convert.ToString(sheet1.cells[row, col + 7].value2);
+
+                // Insert rows to Database
+                PlantOperatingHours(col1, col2, col3, col4, col5, col6, col7, col8);
+                row++;
+                value = Convert.ToString(sheet1.cells[row, col].value2);
+            }
+            connection_close();
+        }
+
+        private void PlantOperatingHours(int PlantOperatingHoursID, int PeriodId,float Value,string created, string modified, string deleted)
+        {
+            // in Excel ID , siloid, componentid, periodid, value, created, modified, deleted
+            // In database RawCleanSiloInventoryID, userid, modelid, componentid, periodid, value, created, modified, deleted, timestamp
+            cmd = new MySqlCommand("INSERT into creat932_scratch.PlantOperatingHours(PlantOperatingHoursId,PeriodId,Value,Created, Modified, Deleted) VALUES(" + PlantOperatingHoursID + "," + PeriodId + "," + Value + ","  + created + "','" + modified + "','" + deleted + ")", conn);
+            //     cmd.Connection = conn;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection_close();
+            }
+
+        }
+
+        private void Get_RawDemandData()
+        {
+            int row = 1;
+            int col = 1;
+
+            int col1;
+            int col2;
+            int col3;
+            float col4;
+            string col5;
+            string col6;
+            string col7;
+            string col8;
+
+            var sheet1 = Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets["Sheet1"];
+            col = 35;
+            row = 3;
+            string value = Convert.ToString(sheet1.cells[row, col].value2);
+            connection();
+            connection_open();
+            while (value != null)
+            {
+                //Get values from excel cells and convert them to appropriate type (should match with column type in database)
+                col1 = Convert.ToInt32(value);
+                col2 = Convert.ToInt32(sheet1.cells[row, col + 1].value2);
+                col3 = Convert.ToInt32(sheet1.cells[row, col + 2].value2);
+                col4 = Convert.ToSingle(sheet1.cells[row, col + 3].value2);
+                col5 = Convert.ToString(sheet1.cells[row, col + 4].value2);
+                col6 = Convert.ToString(sheet1.cells[row, col + 5].value2);
+                col7 = Convert.ToString(sheet1.cells[row, col + 6].value2);
+                col8 = Convert.ToString(sheet1.cells[row, col + 7].value2);
+
+                // Insert rows to Database
+                RawDemand(col1,col3, col4, col5, col6, col7,col8);
+                row++;
+                value = Convert.ToString(sheet1.cells[row, col].value2);
+            }
+            connection_close();
+        }
+
+        private void RawDemand(int RawDemandID, int ComponentId, int PeriodId,float Value , string created, string modified, string deleted)
+        {
+            // in Excel ID , siloid, componentid, periodid, value, created, modified, deleted
+            // In database RawCleanSiloInventoryID, userid, modelid, componentid, periodid, value, created, modified, deleted, timestamp
+            cmd = new MySqlCommand("INSERT into creat932_scratch.RawDemand(RawDemandId,ComponentId,PeriodId,Value,Created, Modified, Deleted) VALUES(" + RawDemandID + "," + ComponentId + "," + PeriodId + ","+ Value+ "," + created + "','" + modified + "','" + deleted + ")", conn);
+            //     cmd.Connection = conn;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection_close();
+            }
+
+        }
+        private void Get_Raw5MToSLRestrictionData()
+        {
+            int row = 1;
+            int col = 1;
+
+            int col1;
+            int col2;
+            int col3;
+            float col4;
+            float col5;
+            string col6;
+            string col7;
+            string col8;
+
+            var sheet1 = Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets["Sheet1"];
+            col = 26;
+            row = 3;
+            string value = Convert.ToString(sheet1.cells[row, col].value2);
+            connection();
+            connection_open();
+            while (value != null)
+            {
+                //Get values from excel cells and convert them to appropriate type (should match with column type in database)
+                col1 = Convert.ToInt32(value);
+                col2 = Convert.ToInt32(sheet1.cells[row, col + 1].value2);
+                col3 = Convert.ToInt32(sheet1.cells[row, col + 2].value2);
+                col4 = Convert.ToSingle(sheet1.cells[row, col + 3].value2);
+                col5 = Convert.ToSingle(sheet1.cells[row, col + 4].value2);
+                col6 = Convert.ToString(sheet1.cells[row, col + 5].value2);
+                col7 = Convert.ToString(sheet1.cells[row, col + 6].value2);
+                col8 = Convert.ToString(sheet1.cells[row, col + 7].value2);
+
+                // Insert rows to Database
+                Raw5MToSLRestriction(col1,col2, col3, col4, col5, col6, col7, col8);
+                row++;
+                value = Convert.ToString(sheet1.cells[row, col].value2);
+            }
+            connection_close();
+        }
+
+        private void Raw5MToSLRestriction(int Raw5MToSLRestrictionID,int SeamId ,int SiloId, float Minimum, float Maximum, string created, string modified, string deleted)
+        {
+            // in Excel ID , siloid, componentid, periodid, value, created, modified, deleted
+            // In database RawCleanSiloInventoryID, userid, modelid, componentid, periodid, value, created, modified, deleted, timestamp
+            cmd = new MySqlCommand("INSERT into creat932_scratch.Raw5MToSLRestriction(Raw5MToSLRestrictionId,SeamId,SiloId,Minimum,Maximum,Created, Modified, Deleted) VALUES(" + Raw5MToSLRestrictionID + ","+ SeamId + "," + SiloId + ","  + Minimum + ",'" + Maximum + "','" + created + "','" + modified + "','" + deleted + ")", conn);
+            //     cmd.Connection = conn;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection_close();
+            }
+
+        }
+
+
+        private void Get_RawCMToCSRestrictionData()
+        {
+            int row = 1;
+            int col = 1;
+
+            int col1;
+            int col2;
+            int col3;
+            float col4;
+            float col5;
+            string col6;
+            string col7;
+            string col8;
+
+            var sheet1 = Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets["Sheet1"];
+            col = 17;
+            row = 3;
+            string value = Convert.ToString(sheet1.cells[row, col].value2);
+            connection();
+            connection_open();
+            while (value != null)
+            {
+                //Get values from excel cells and convert them to appropriate type (should match with column type in database)
+                col1 = Convert.ToInt32(value);
+                col2 = Convert.ToInt32(sheet1.cells[row, col + 1].value2);
+                col3 = Convert.ToInt32(sheet1.cells[row, col + 2].value2);
+                col4 = Convert.ToSingle(sheet1.cells[row, col + 3].value2);
+                col5 = Convert.ToSingle(sheet1.cells[row, col + 4].value2);
+                col6 = Convert.ToString(sheet1.cells[row, col + 5].value2);
+                col7 = Convert.ToString(sheet1.cells[row, col + 6].value2);
+                col8 = Convert.ToString(sheet1.cells[row, col + 7].value2);
+
+                // Insert rows to Database
+                RawCMToCSRestriction(col1, col3, col4, col5, col6, col7, col8);
+                row++;
+                value = Convert.ToString(sheet1.cells[row, col].value2);
+            }
+            connection_close();
+        }
+
+        private void RawCMToCSRestriction(int RawCMToCSRestrictionID, int SiloId, int componentID, float Minimum, float Maximum, string created, string modified, string deleted)
+        {
+            // in Excel ID , siloid, componentid, periodid, value, created, modified, deleted
+            // In database RawCleanSiloInventoryID, userid, modelid, componentid, periodid, value, created, modified, deleted, timestamp
+            cmd = new MySqlCommand("INSERT into creat932_scratch.RawCMToCSRestriction(RawCMToCSRestrictionId,SiloId,ComponentId,Minimum,Maximum,Created, Modified, Deleted) VALUES(" + RawCMToCSRestrictionID + "," + SiloId + "," + componentID + "," + Minimum + ",'" + Maximum + "','" + created + "','" + modified + "','" + deleted + ")", conn);
+            //     cmd.Connection = conn;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection_close();
+            }
+
+
+        }
         private void Get_RawCleanSiloInventoryData()
         {
             int row = 1;
             int col = 1;
+            
             int col1;
             int col2;
             int col3;
@@ -86,6 +332,8 @@ namespace ExcelAddIn3
 
         }
 
+        
+
         private void connection()
         {
             connectionstring = new MySqlConnectionStringBuilder();
@@ -114,6 +362,10 @@ namespace ExcelAddIn3
         private void button1_Click_1(object sender, EventArgs e)
         {
             Get_RawCleanSiloInventoryData();
+            Get_RawCMToCSRestrictionData();
+            Get_Raw5MToSLRestrictionData();
+            Get_RawDemandData();
+            Get_PlantOperatingHoursData();
 
         }
 
